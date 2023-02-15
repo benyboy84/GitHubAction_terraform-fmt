58e309d67f4ebd5033bf13ef59477942352dac79
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-02-15 07:44:22 -0500

    refactor: variable name

--- main.sh
+++ main.sh
@@ -15,7 +15,7 @@ IS_ARRAY()
 
 # `PULL_REQUEST_COMMENT` function will create a comment if the action is call from a pull request.
 # If a comment already exist in the pull request, it will delete it and create a new one.
-# If there EXITCODE variable is set to 0, meaning that there is no error, if a comment exist, it will be deleted.
+# If there `exit_code` variable is set to 0, meaning that there is no error, if a comment exist, it will be deleted.
 # @param: pull request comment
 #    Follow this guide to build the string for the body of the pull request comment:
 #    https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheets
@@ -29,22 +29,22 @@ PULL_REQUEST_COMMENT () {
         else
             # Look for an existing pull request comment and delete
             echo "INFO     | Looking for an existing pull request comment."
-            local ACCEPT_HEADER="Accept: application/vnd.github.v3+json"
-            local AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
-            local CONTENT_HEADER="Content-Type: application/json"
+            local accept_header="Accept: application/vnd.github.v3+json"
+            local auth_header="Authorization: token $GITHUB_TOKEN"
+            local content_header="Content-Type: application/json"
             if [[ "$GITHUB_EVENT_NAME" == "issue_comment" ]]; then
-                local PR_COMMENTS_URL=$(jq -r ".issue.comments_url" "$GITHUB_EVENT_PATH")
+                local pr_comments_url=$(jq -r ".issue.comments_url" "$GITHUB_EVENT_PATH")
             else
-                local PR_COMMENTS_URL=$(jq -r ".pull_request.comments_url" "$GITHUB_EVENT_PATH")
+                local pr_comments_url=$(jq -r ".pull_request.comments_url" "$GITHUB_EVENT_PATH")
             fi
-            local PR_COMMENT_URI=$(jq -r ".repository.issue_comment_url" "$GITHUB_EVENT_PATH" | sed "s|{/number}||g")
-            local PR_COMMENT_ID=$(curl -sS -H "$AUTH_HEADER" -H "$ACCEPT_HEADER" -L "$PR_COMMENTS_URL" | jq '.[] | select(.body|test ("### Terraform Format")) | .id')
-            if [ "$PR_COMMENT_ID" ]; then
-                if [[ $(IS_ARRAY $PR_COMMENT_ID)  -ne 0 ]]; then
-                    echo "INFO     | Found existing pull request comment: $PR_COMMENT_ID. Deleting."
-                    local PR_COMMENT_URL="$PR_COMMENT_URI/$PR_COMMENT_ID"
+            local pr_comment_uri=$(jq -r ".repository.issue_comment_url" "$GITHUB_EVENT_PATH" | sed "s|{/number}||g")
+            local pr_comment_id=$(curl -sS -H "$auth_header" -H "$accept_header" -L "$pr_comments_url" | jq '.[] | select(.body|test ("### Terraform Format")) | .id')
+            if [ "$pr_comment_id" ]; then
+                if [[ $(IS_ARRAY $pr_comment_id)  -ne 0 ]]; then
+                    echo "INFO     | Found existing pull request comment: $pr_comment_id. Deleting."
+                    local pr_comment_url="$pr_comment_uri/$pr_comment_id"
                     {
-                        curl -sS -X DELETE -H "$AUTH_HEADER" -H "$ACCEPT_HEADER" -L "$PR_COMMENT_URL" > /dev/null
+                        curl -sS -X DELETE -H "$auth_header" -H "$accept_header" -L "$pr_comment_url" > /dev/null
                     } ||
                     {
                         echo "ERROR    | Unable to delete existing comment in pull request."
@@ -56,14 +56,14 @@ PULL_REQUEST_COMMENT () {
             else
                 echo "INFO     | No existing pull request comment found."
             fi
-            if [[ $EXITCODE -ne 0 ]]; then
+            if [[ $exit_code -ne 0 ]]; then
                 # Add comment to pull request.
-                BODY="### Terraform Format Failed
+                local body="### Terraform Format Failed
 $1"
-                local PR_PAYLOAD=$(echo '{}' | jq --arg body "$BODY" '.body = $body')
+                local pr_payload=$(echo '{}' | jq --arg body "$body" '.body = $body')
                 echo "INFO     | Adding comment to pull request."
                 {
-                    curl -sS -X POST -H "$AUTH_HEADER" -H "$ACCEPT_HEADER" -H "$CONTENT_HEADER" -d "$PR_PAYLOAD" -L "$PR_COMMENTS_URL" > /dev/null
+                    curl -sS -X POST -H "$auth_header" -H "$accept_header" -H "$content_header" -d "$pr_payload" -L "$pr_comments_url" > /dev/null
                 } ||
                 {
                     echo "ERROR    | Unable to add comment to pull request."
@@ -76,40 +76,40 @@ $1"
 # Optional inputs
 
 # Validate input target.
-TARGET=""
+target=""
 if [[ -n "$INPUT_TARGET" ]]; then
     if [[ -d "$INPUT_TARGET" || -f "$INPUT_TARGET" ]]; then
-        TARGET=$INPUT_TARGET
+        target=$INPUT_TARGET
     else
-        EXITCODE=1
+        exit_code=1
         echo "ERROR    | Target does not exist: \"$INPUT_TARGET\"."
-        PR_COMMENT="Provided value \"$INPUT_TARGET\" for \`target\` input does not exist. 
+        pr_comment="Provided value \"$INPUT_TARGET\" for \`target\` input does not exist. 
 You need to provide an existing file or directory."
-        PULL_REQUEST_COMMENT "$PR_COMMENT"
-        exit $EXITCODE
+        PULL_REQUEST_COMMENT "$pr_comment"
+        exit $exit_code
     fi
 fi
 
 # Validate input recursive.
-RECURSIVE=""
+recursive=""
 if [[ ! "$INPUT_RECURSIVE" =~ ^(true|false)$ ]]; then
-    EXITCODE=1
+    exit_code=1
     echo "ERROR    | Unsupported command \"$INPUT_RECURSIVE\" for input \"Recursive\". Valid values are \"true\" or \"false\"."
-    PR_COMMENT="Unsupported command \"$INPUT_RECURSIVE\" for input \`recursive\` input. 
+    pr_comment="Unsupported command \"$INPUT_RECURSIVE\" for input \`recursive\` input. 
 Valid values are \"true\" or \"false\"."
-        PULL_REQUEST_COMMENT "$PR_COMMENT"
-        exit $EXITCODE
+        PULL_REQUEST_COMMENT "$pr_comment"
+        exit $exit_code
 fi
 if [[ "$INPUT_RECURSIVE" == true ]]; then
-    RECURSIVE="-recursive"
+    recursive="-recursive"
 fi
 
 # Detect terraform version
-VERSION=$(terraform version -json | jq -r '.terraform_version' 2>/dev/null || terraform version | grep 'Terraform v' | sed 's/Terraform v//')
-if [[ -z $VERSION  ]]; then
-    EXITCODE=1
+version=$(terraform version -json | jq -r '.terraform_version' 2>/dev/null || terraform version | grep 'Terraform v' | sed 's/Terraform v//')
+if [[ -z $version  ]]; then
+    exit_code=1
     echo "ERROR    | Terraform not detected."
-    PR_COMMENT="<details><summary>Show Output</summary>
+    pr_comment="<details><summary>Show Output</summary>
 <p>
 This GitHub Actions does not install \`terraform\`, so you have to install them in advanced.
 
@@ -122,37 +122,37 @@ with:
 
 </p>
 </details>"
-        PULL_REQUEST_COMMENT "$PR_COMMENT"
-        exit $EXITCODE
+        PULL_REQUEST_COMMENT "$pr_comment"
+        exit $exit_code
 else
-    echo "INFO     | Using terraform version $VERSION."
+    echo "INFO     | Using terraform version $version."
 fi
 
 # Gather the output of `terraform fmt`.
-OUTPUT=$(terraform fmt -list=false -check ${RECURSIVE} ${TARGET})
-EXITCODE=${?}
+output=$(terraform fmt -list=false -check ${recursive} ${target})
+exit_code=${?}
 
 # Exit Code: 0
 # Meaning: All files formatted correctly.
-if [[ $EXITCODE -eq 0 ]]; then
+if [[ $exit_code -eq 0 ]]; then
     echo "INFO     | Terraform file(s) are correctly formatted"
 fi
 
 # Exit Code: 1, 2
 # Meaning: 1 = Malformed Terraform CLI command. 2 = Terraform parse error.
 # Actions: Build pull request comment.
-if [[ $EXITCODE -eq 1 || $EXITCODE -eq 2 ]]; then
-    if [[ $EXITCODE -eq 2 ]]; then
+if [[ $exit_code -eq 1 || $exit_code -eq 2 ]]; then
+    if [[ $exit_code -eq 2 ]]; then
         echo "ERROR    | Failed to parse terraform file(s)."
     else
         echo "ERROR    | Malformed terraform CLI command."
     fi
     # Add output of `terraform fmt` command.
     echo -e "ERROR    | Terraform fmt output:"
-    echo -e $OUTPUT
-    PR_COMMENT="<details><summary>Show Output</summary>
+    echo -e $output
+    pr_comment="<details><summary>Show Output</summary>
 <p>
-$OUTPUT
+$output
 </p>
 </details>"
 fi
@@ -160,35 +160,35 @@ fi
 # Exit Code: 3
 # Meaning: One or more files are incorrectly formatted.
 # Actions: Iterate over all files and build diff-based pull request comment.
-if [[ $EXITCODE -eq 3 ]]; then
+if [[ $exit_code -eq 3 ]]; then
     echo "ERROR    | Terraform file(s) are incorrectly formatted."
     # Add output of `terraform fmt` command.
     echo -e "ERROR    | Terraform fmt output:"
-    echo -e $OUTPUT
-    ALL_FILES_DIFF=""
-    OUTPUT=""
-    FILES=$(terraform fmt -check -write=false -list ${RECURSIVE})
-    for FILE in $FILES; do
-        THIS_FILE_DIFF=$(terraform fmt -no-color -write=false -diff "$FILE")
-        ALL_FILES_DIFF="$ALL_FILES_DIFF
-<details><summary><code>$FILE</code></summary>
+    echo -e $output
+    all_files_diff=""
+    output=""
+    files=$(terraform fmt -check -write=false -list ${recursive})
+    for file in $files; do
+        this_file_diff=$(terraform fmt -no-color -write=false -diff "$file")
+        all_files_diff="$all_files_diff
+<details><summary><code>$file</code></summary>
 <p>
 
 \`\`\`diff
-$THIS_FILE_DIFF
+$this_file_diff
 \`\`\`
 
 </p>
 </details>"
-        OUTPUT="$OUTPUT
+        output="$output
 
-$THIS_FILE_DIFF"
+$this_file_diff"
     done
     echo -e "ERROR    | Terraform fmt output:"
-    echo -e "$OUTPUT"
-    PR_COMMENT="$ALL_FILES_DIFF"
+    echo -e "$output"
+    pr_comment="$all_files_diff"
 fi
 
-PULL_REQUEST_COMMENT "$PR_COMMENT"
+PULL_REQUEST_COMMENT "$pr_comment"
 
-exit $EXITCODE
+exit $exit_code
